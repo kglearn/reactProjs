7a5949772379d80bf778556f7cd653a574fada9b
Kuber Gaur <kay2gt@gmail.com>
2022-01-18 10:53:54 +0530

    peep counter

--- js/peepCounter/index.js
+++ js/peepCounter/index.js
@@ -1,1 +1,14 @@
-document.getElementById("count-el").innerText = 5
+// 
+
+let countEl = document.getElementById("count-el")
+let entriesEl = document.getElementById("entries-el")
+
+function increment() {
+    countEl.textContent = parseInt(countEl.textContent) + 1
+}
+
+function save() {
+    entriesEl.textContent += countEl.textContent + " - " 
+    countEl.textContent = 0
+}
+
